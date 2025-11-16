85d6446c6c2a452c75564644012f3a2601e6a88f
Andrew Scott <scottmeup@users.noreply.github.com>
2025-11-16 21:06:55 +1100

    Create network-monitor.sh

---
+++ bash/pia-router/network-monitor.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#logic to disable IP forwarding is in the checking scripts
+#logic to enable IP forwarding is in this script
+
+#assign the scripts to run to variables
+check1="/opt/script/piastatuscheck"
+check2="/opt/script/checkfirsthop"
+
+# Monitor IP address, route, and link changes
+ip monitor address route link | while read line; do
+
+# Run both checks asynchronously
+check1 & pid1=check1 & pid1=!
+check2 & pid2=check2 & pid2=!
+
+# Wait for both to finish
+wait $pid1
+status1=$?
+
+wait $pid2
+status2=$?
+
+# Check if both exit statuses are 0
+if [[ status1 -eq 0 &&status1 -eq 0 && status2 -eq 0 ]]; then
+    # Enable IP forwarding
+    echo "Both checks succeeded. Enabling IP Forwarding"   
+    sysctl -w net.ipv4.ip_forward=1 
+else
+    echo "One or both checks failed."
+    echo "Check 1 exit code: $status1"
+    echo "Check 2 exit code: $status2"
+fi
+
+done
