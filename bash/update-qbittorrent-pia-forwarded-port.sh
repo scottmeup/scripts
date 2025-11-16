4695b53798e4ef99f8e26328be2c4f3033e8b3b2
Andrew Scott <scottmeup@users.noreply.github.com>
2025-11-16 15:11:11 +1100

    Update update-qbittorrent-pia-forwarded-port.sh

--- bash/update-qbittorrent-pia-forwarded-port.sh
+++ bash/update-qbittorrent-pia-forwarded-port.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Update qBittorrent on localhost with active forwarded port from piactl  # 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Update qBittorrent with active forwarded port from piactl # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-#qBittorrent Web UI port
-WEBUI_PORT=8080 
+#qBittorrent Web UI config
+WEBUI_HOST=localhost
+WEBUI_PORT=8080
 
 #read PIA forwarded port
-forwarded_port=$(piactl get portforward)
+FORWARDED_PORT=$(piactl get portforward)
 
-curl -k -i -X POST -d "json={\"random_port\": false}" "http://localhost:${WEBUI_PORT}/api/v2/app/setPreferences"
-curl -k -i -X POST -d "json={\"listen_port\": {forwarded_port}}" "http://localhost:{forwarded_port}}" "http://localhost:{WEBUI_PORT}/api/v2/app/setPreferences"
+curl -k -i -X POST -d "json={\"random_port\": false}" "http://${WEBUI_HOST}:${WEBUI_PORT}/api/v2/app/setPreferences"
+curl -k -i -X POST -d "json={\"listen_port\": ${FORWARDED_PORT}}" "http://${WEBUI_HOST}:${WEBUI_PORT}/api/v2/app/setPreferences"
